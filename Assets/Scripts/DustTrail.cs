c944df9f78af3daa7227e6fc5d29707d007c89ab
Philippe <philippe.perfetto@hotmail.fr>
2022-11-06 20:36:08 +0100

    Unity updated

--- Assets/Scripts/DustTrail.cs
+++ Assets/Scripts/DustTrail.cs
@@ -11,7 +11,7 @@ public class DustTrail : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D other) {
         snowParticles.Play();
-        snowSound.Play(5000);
+        snowSound.PlayDelayed(5000);
 
     }
 
